7f11ed8eccb65d8c5db8d019117b5112282f7166
Tiago19480521 <146497077+Tiago19480521@users.noreply.github.com>
2025-12-31 22:03:25 -0300

    Editing JS file

--- Git_Test_1.js
+++ Git_Test_1.js
@@ -1,5 +1,5 @@
 
 document.getElementById('par_title').innerHTML = 'GIT tests in HTML'
 document.getElementById('par_init').innerHTML = 'Testing GIT commands.'
-document.getElementById('par_test1').innerHTML = 'This is small text.'
-document.getElementById('par_test2').innerHTML = 'This is large text.'
+document.getElementById('par_test1').innerHTML = 'This is a small text.'
+document.getElementById('par_test2').innerHTML = 'This is a large text.'
