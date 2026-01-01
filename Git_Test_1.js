4c4c1796eba5eb278e180a8cb9a135148cea68fb
Tiago19480521 <146497077+Tiago19480521@users.noreply.github.com>
2025-12-31 21:34:22 -0300

    Editing files 3

---
+++ Git_Test_1.js
@@ -0,0 +1,5 @@
+
+document.getElementById('par_title').innerHTML = 'GIT tests in HTML'
+document.getElementById('par_init').innerHTML = 'Testing GIT commands.'
+document.getElementById('par_test1').innerHTML = 'This is small text.'
+document.getElementById('par_test2').innerHTML = 'This is large text.'
